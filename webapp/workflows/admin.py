dfc09b567fefff0eeb7ef193dffe86e7795c5343
Vadim Ovcearenco <wadim@veles-soft.com>
2018-03-14 18:30:46 +0200

    Add workflows app and migrations

--- webapp/workflows/admin.py
+++ webapp/workflows/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from . import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+admin.site.register(models.QuestionType)
+admin.site.register(models.Answer)
+
+
+class QuestionInline(admin.TabularInline):
+    model = models.Question
+    ordering = ['order']
+    readonly_fields = ('_link', )
+
+    def _link(self, obj):
+        if obj.pk:  # if object has already been saved and has a primary key, show link to it
+            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[force_text(obj.pk)])
+            return mark_safe("""<a href="{url}">{text}</a>""".format(
+                url=url,
+                text=_("Edit"),
+            ))
+        return _("-")
+
+
+
+class WorkflowAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionInline
+    ]
+
+
+admin.site.register(models.Workflow, WorkflowAdmin)
+
+
+class AnswerInline(admin.TabularInline):
+    model = models.Answer
+    fk_name = 'question'
+    ordering = ['order']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        AnswerInline
+    ]
+
+
+admin.site.register(models.Question, QuestionAdmin)
--- webapp/workflows/models.py
+++ webapp/workflows/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Workflow(models.Model):
+    slug = models.SlugField(unique=True, null=False, blank=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class QuestionType(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    slug = models.SlugField(null=False, blank=False)
+    question_type = models.ForeignKey(QuestionType, blank=False, null=False, on_delete=models.PROTECT)
+    text = models.CharField(max_length=1000, blank=False, null=False)
+    workflow = models.ForeignKey(Workflow, blank=False, null=False, on_delete=models.CASCADE)
+    order = models.IntegerField(blank=False, null=False)
+
+    class Meta:
+        unique_together = ('slug', 'workflow',)
+
+    def __str__(self):
+        return self.slug
+
+
+class Answer(models.Model):
+    slug = models.SlugField(null=False, blank=False)
+    question = models.ForeignKey(Question, blank=False, null=False, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000, blank=False, null=False)
+    next_question = models.ForeignKey(Question, blank=True, null=True, on_delete=models.PROTECT, related_name='next_questions')
+    order = models.IntegerField(blank=False, null=False)
+
+    class Meta:
+        unique_together = ('slug', 'question',)
+
+    def __str__(self):
+        return self.slug
