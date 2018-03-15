6029b297b4f4f6ec60a1f10250ea1a77386836e6
Vadim Ovcearenco <wadim@veles-soft.com>
2018-03-15 09:03:19 +0200

    Nested inlines for workflows admin

--- webapp/workflows/admin.py
+++ webapp/workflows/admin.py
@@ -1,50 +1,29 @@
 from django.contrib import admin
+import nested_admin
 from . import models
-from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import force_text
-from django.utils.safestring import mark_safe
 
 
 admin.site.register(models.QuestionType)
-admin.site.register(models.Answer)
+
+class AnswerInline(nested_admin.NestedTabularInline):
+    model = models.Answer
+    fk_name = 'question'
+    ordering = ['order']
 
 
-class QuestionInline(admin.TabularInline):
+class QuestionInline(nested_admin.NestedTabularInline):
     model = models.Question
     ordering = ['order']
-    readonly_fields = ('_link', )
-
-    def _link(self, obj):
-        if obj.pk:  # if object has already been saved and has a primary key, show link to it
-            url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[force_text(obj.pk)])
-            return mark_safe("""<a href="{url}">{text}</a>""".format(
-                url=url,
-                text=_("Edit"),
-            ))
-        return _("-")
+    inlines = [
+        AnswerInline
+    ]
 
 
 
-class WorkflowAdmin(admin.ModelAdmin):
+class WorkflowAdmin(nested_admin.NestedModelAdmin):
     inlines = [
         QuestionInline
     ]
 
 
 admin.site.register(models.Workflow, WorkflowAdmin)
-
-
-class AnswerInline(admin.TabularInline):
-    model = models.Answer
-    fk_name = 'question'
-    ordering = ['order']
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [
-        AnswerInline
-    ]
-
-
-admin.site.register(models.Question, QuestionAdmin)
