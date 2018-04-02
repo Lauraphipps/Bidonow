9c165c799c2fadd7558602fe2bed4c0153414b3e
Vadim Ovcearenco <wadim@veles-soft.com>
2018-04-02 12:02:37 +0300

    Add google authentication integration

--- webapp/settings/common.py
+++ webapp/settings/common.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.linkedin',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
